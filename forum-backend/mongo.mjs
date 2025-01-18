523c10c3782abc57950a7fc6e4bbd3c6212197dd
Aryan Chegini <cheginiaryan.ac@gmail.com>
2025-01-18 19:24:40 +0000

    fix

--- forum-backend/mongo.mjs
+++ forum-backend/mongo.mjs
@@ -1,6 +1,9 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
-const uri = "mongodb+srv://aryanchegini:Ho4jCTRTBje6Od3b@cluster0.96iwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
